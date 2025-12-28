3ae4857a14641c164829003ef2820f4cbf630f52
Asomiddin <120255236+Asomiddin1@users.noreply.github.com>
2025-12-28 19:53:37 +0500

    safsa

--- resources/views/admin/groups/settings-modal.blade.php
+++ resources/views/admin/groups/settings-modal.blade.php
@@ -35,7 +35,7 @@
             </button>
         </form>
 
-        <!-- 2️⃣ STATUS -->
+        
         <form method="POST" action="{{ route('admin.groups.status', $group->id) }}" class="mb-4">
             @csrf
             @method('PATCH')
@@ -60,7 +60,7 @@
             </button>
         </form>
 
-        <!-- 3️⃣ DELETE GROUP -->
+       
         <form method="POST"
               action="{{ route('admin.groups.destroy', $group->id) }}"
               onsubmit="return confirm('Guruhni o‘chirishga ishonchingiz komilmi?')">
